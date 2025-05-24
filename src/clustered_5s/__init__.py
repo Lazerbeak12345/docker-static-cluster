f17a7e3d208849d40c9f94597b443e4291bd2bbf
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-05-24 15:18:03 -0600

    feat: start using click

--- src/clustered_5s/__init__.py
+++ src/clustered_5s/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+import click
+
+@click.group()
 def main():
-    print("main")
+    pass
+
+@main.command()
+def generate():
+    raise NotImplementedError(generate)
 
 if __name__ == "__main__":
     main()

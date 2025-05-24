d39caf1d390490f722680786d06362d02f719201
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-05-24 17:23:55 -0600

    feat: initial command

--- src/clustered_5s/__init__.py
+++ src/clustered_5s/__init__.py
@@ -1,13 +1,56 @@
 #!/usr/bin/env python3
+import os
+import sys
+import tomllib
+
 import click
+import schema
+
+# TODO: https://click.palletsprojects.com/en/stable/shell-completion/
+
 
 @click.group()
+@click.version_option()
 def main():
     pass
 
+
+config_schema = schema.Schema(
+    {
+        "clustered_5s_config_version": "0.0.0",
+    }
+)
+
+
 @main.command()
-def generate():
-    raise NotImplementedError(generate)
+@click.option(
+    "-f",
+    "--file",
+    "--infile",
+    "infile",
+    #prompt=True,
+    default="clustered_5s.toml",
+    type=click.File("rb"),
+)
+@click.option(
+    "-o",
+    "--outfile",
+    #prompt=True,
+    default=lambda: os.environ.get("COMPOSE_FILE", "compose.yaml").split(
+        os.environ.get("COMPOSE_PATH_SEPARATOR", ":")
+    )[0],
+    type=click.File("w"),
+)
+def generate_compose(infile, outfile):
+    """Generate a compose file for use with `docker stack`"""
+    try:
+        config = config_schema.validate(tomllib.load(infile))
+    except schema.SchemaError as e:
+        click.echo(f"error in config file {infile.name}\n{e}")
+        sys.exit(1)
+    click.echo(config)
+    raise NotImplementedError(generate_compose)
+
 
 if __name__ == "__main__":
     main()
